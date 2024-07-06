16f738114ce0845f6bad3c07d87abf0a3f0f6b1e
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-07-06 15:12:54 +0530

    name space added

--- web2.py
+++ web2.py
@@ -15,19 +15,19 @@ df = pandas.read_csv("data (1).csv", sep=",")
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.subheader(f'{row["first name"].title()}{row["last name"].title()}')
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/" + row["image"])
 
 with col2:
     for index, row in df[4:8].iterrows():
-        st.subheader(f'{row["first name"].title()}{row["last name"].title()}')
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/" + row["image"])
 
 with col3:
     for index, row in df[8:].iterrows():
-        st.subheader(f'{row["first name"].title()}{row["last name"].title()}')
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
         st.write(row["role"])
         st.image("images/" + row["image"])
 
